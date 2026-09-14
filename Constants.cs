a283caf870a01d5525537f900e2694aaef80c2f8
recgoblingu-lgtm <recgoblingu@gmail.com>
2026-09-13 22:49:39 -0500

    Add Constants class for API routes

---
+++ Constants.cs
@@ -0,0 +1,8 @@
+namespace Arphix;
+
+public static class Constants
+{
+    public const string RoomRoute = "/api/rooms/v1/";
+    public const string AvatarRoute = "/api/avatar/v1/";
+    public const string AccountRoute = "/api/accounts/v1/";
+}
